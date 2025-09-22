05fb4e6c6cee091b7d18e92dd060cb50edf4cf8f
MedievAlex <alexirazola@gmail.com>
2025-09-22 10:06:23 +0200

    Managed to order the windows and their controlers

---
+++ Din_Reto_1_Factory/src/view/FXMLWindowLoginController.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class FXMLWindowLoginController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private Text lblNombreDeUsuario;
+    @FXML
+    private Text lblContraseña;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private CheckBox cbxBaseDeDatos;
+    @FXML
+    private CheckBox cbxFichero;
+    @FXML
+    private TextField txtNombreDeUsuario;
+    @FXML
+    private PasswordField txtContraseña;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
