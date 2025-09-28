bb16e06f656b02768f2ef3a5c2f6646cac597704
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2025-09-28 21:38:03 +0200

    Modificando ventanas

--- Din_Reto_1_Factory/src/view/FXMLWindowLoginController.java
+++ Din_Reto_1_Factory/src/view/FXMLWindowLoginController.java
@@ -11,87 +11,63 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
- * @author Alex Irazola & Ekaitz Campo
+ * Controlador de la ventana de Login
  */
 public class FXMLWindowLoginController implements Initializable {
 
     private Controller controller;
-    @FXML
-    private Button btnLogin;
-    @FXML
-    private CheckBox cbxBaseDeDatos;
-    @FXML
-    private CheckBox cbxFichero;
-    @FXML
-    private TextField txtNombreDeUsuario;
-    @FXML
-    private PasswordField txtContraseña;
-    @FXML
-    private Text lblNombreDeUsuario;
-    @FXML
-    private Text lblContraseña;
-    @FXML
-    private Text lblTitle;
-    @FXML
-    private Text lblErrorMessage;
+
+    @FXML private ToggleGroup Method;
+    @FXML private Button btnLogin;
+    @FXML private Text lblContraseña;
+    @FXML private Text lblErrorMessage;
+    @FXML private Text lblNombreDeUsuario;
+    @FXML private Text lblTitle;
+    @FXML private RadioButton rbBaseDeDatos;
+    @FXML private RadioButton rbFichero;
+    @FXML private PasswordField txtContraseña;
+    @FXML private TextField txtNombreDeUsuario;
 
     /**
-     * Sets the controller.
-     *
-     * @param controller
-     */    
+     * Asigna el controlador principal
+     */
     public void setController(Controller controller) {
         this.controller = controller;
     }
 
     /**
-     * ...
-     *
-     * @param user
+     * Método llamado al pulsar el botón LOGIN
      */
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-       // label.setText("Hello World!");
-    }
-
-    /**
-     * Opens the next window where the information will be shown.
-     *
-     * @param event
-     * @param stage
-     * @exception IOException
-     */    
-    public void openWindowShow(ActionEvent event, Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/WindowShow.fxml"));
-        Parent root = loader.load();
-
-        FXMLWindowLoginController loginController = loader.getController();
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * ...
-     *
-     * @param url
-     * @param rb
-     */     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
     @FXML
     private void openWindowShow(ActionEvent event) {
+        try {
+            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/WindowShow.fxml"));
+            Parent root = loader.load();
+
+            FXMLWindowShowController showController = loader.getController();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            lblErrorMessage.setText("Error al abrir la ventana.");
+        }
     }
-    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        lblErrorMessage.setText("");
+    }
 }
